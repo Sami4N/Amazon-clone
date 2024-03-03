23b78042d89fc02bd5ba2dc1ca21303a7b6ad3eb
Samuel Negasa <samuelnegasa2@gmail.com>
2024-03-03 07:36:42 -0800

    Update Home page and Navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,23 @@ import { IoSearchSharp } from "react-icons/io5";
 import { FaCaretDown } from "react-icons/fa";
 import { PiShoppingCartSimpleBold } from "react-icons/pi";
 import { IoMenu } from "react-icons/io5";
-
+import { FaUser } from "react-icons/fa";
+import { IoIosArrowForward } from "react-icons/io";
+import { TbWorld } from "react-icons/tb";
+import { LiaFlagUsaSolid } from "react-icons/lia";
+import { IoMdClose } from "react-icons/io";
 
 const Header = () => {
   const [search, setSearch] = useState('')
+  const [nav, setNav] = useState(false)
+  const [hov, setHov] = useState(true)
+
+  const handleNav = () =>{
+    setNav(!nav)
+}
+const handleHover = () =>{
+  setHov(!hov)
+}
   return (
     <>
     <div className='w-full myblack h-20 text-white flex justify-around'>
@@ -23,26 +36,118 @@ const Header = () => {
           <input className='w-[100%] mx-20 mr-0 p-3 rounded-md rounded-r-none placeholder:text-gray-500 font-medium focus:outline-none focus:ring-2 focus:ring-[#ea8a0c]' type="search" value={search} onChange={(event) => setSearch(event.target.value)} placeholder='Search Amazon'/>
           <IoSearchSharp className='text-gray-600 w-[50px] h-[48px] px-[10px] bg-[#FEBD69] rounded-r-md' />
       </div>
-      <div className='w-[30%] flex justify-between items-center mx-10'>
-        <div className='flex items-end'>
-          <h4 className='text-sm'>Hello, sign in <br /><span className=' text-md font-bold'>Account & Lists</span></h4>
-          <FaCaretDown />
+      <div className='w-[40%] flex justify-between items-center mx-10'>
+        <div className='relative'>
+            <div className='flex items-end  hover:border-[1px] hover:py-2 px-2 hover:cursor-pointer dropdown'>
+              <h4 className='text-sm'>Hello, sign in <br /><span className=' text-md font-bold'>Account & Lists</span></h4>
+              <FaCaretDown />
+            </div>
+              <div className='w-[35rem] fixed top-[70px] right-[-100%] bg-slate-300 text-black rounded-sm  drop ease-in-out duration-700'>
+                <div className='flex flex-col items-center py-4 mx-6 border-b-[1px]'>
+                  <button className='myyellow px-[6rem] py-2 rounded-lg'>Sign in</button>
+                  <p className='text-[13px]'>New customer? <a className=' text-blue-500 hover:text-red-800 hover:underline' href="#">Start here</a></p>
+                </div>
+                <div  className='grid grid-cols-2 py-4'>
+                  <div className=' border-r-2'>
+                    <h1 className='font-bold text-[19px] px-6 mb-3'>Your Lists</h1>
+                    <ul>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Create a List</li>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Find a List or Registry</li>
+                    </ul>
+                  </div>
+                  <div>
+                    <h1 className='font-bold text-[19px] px-6 mb-3'>Your Account</h1>
+                    <ul>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Account</li>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Orders</li>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Recommendations</li>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Browsing History</li>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Watchlist</li>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Video Purchases & Rentals</li>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Kindle Unlimited</li>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Content & Devices</li>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Subscribe & Save Items</li>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Memberships & Subscription</li>
+                      <li className='px-6 hover:cursor-pointer hover:text-red-700 hover:underline'>Music Library</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
         </div>
-        <h4 className='text-sm'>Returns <br /><span className=' text-md font-bold'>& Orders</span></h4>
-        <div><PiShoppingCartSimpleBold size={40}/></div>
+        <h4 className='text-sm hover:border-[1px] hover:py-2 px-2 hover:cursor-pointer'>Returns <br /><span className=' text-md font-bold'>& Orders</span></h4>
+        <div className='flex items-end font-semibold  hover:border-[1px] hover:py-2 px-2 hover:cursor-pointer'><PiShoppingCartSimpleBold size={40}/>Cart</div>
       </div>
     </div>
     <div className='w-full myblue h-11 text-white flex justify-start items-center p-2'>
-      <div className='flex items-center hover:border-[1px] hover:py-2 px-2'>
+      <div onClick={handleNav} className='flex items-center hover:border-[1px] hover:py-1 px-2 hover:cursor-pointer'>
         <IoMenu size={35}/>
         <p className='mx-3 ml-0 font-bold'>All</p>
       </div>
-      <div className='mx-1 font-medium hover:border-[1px] hover:py-2 px-2'>Today's Deals</div>
-      <div className='mx-1 font-medium hover:border-[1px] hover:py-2 px-2'>Customer Service</div>
-      <div className='mx-1 font-medium hover:border-[1px] hover:py-2 px-2'>Registry</div>
-      <div className='mx-1 font-medium hover:border-[1px] hover:py-2 px-2'>Gift Cards</div>
-      <div className='mx-1 font-medium hover:border-[1px] hover:py-2 px-2'>Sell</div>
+      <div className='mx-1 font-medium hover:border-[1px] hover:py-2 px-2 hover:cursor-pointer'>Today's Deals</div>
+      <div className='mx-1 font-medium hover:border-[1px] hover:py-2 px-2 hover:cursor-pointer'>Customer Service</div>
+      <div className='mx-1 font-medium hover:border-[1px] hover:py-2 px-2 hover:cursor-pointer'>Registry</div>
+      <div className='mx-1 font-medium hover:border-[1px] hover:py-2 px-2 hover:cursor-pointer'>Gift Cards</div>
+      <div className='mx-1 font-medium hover:border-[1px] hover:py-2 px-2 hover:cursor-pointer'>Sell</div>
     </div>
+    <div className={nav ?' bg-black fixed top-0 left-0 w-full h-full bg-opacity-40  ease-in-out duration-700' : 'fixed left-[-100%]'}>
+    <div className={nav ? 'w-[25rem] bg-white text-black fixed top-0 left-0 ease-in-out duration-700' : 'fixed left-[-100%]'}>
+      <div className='flex justify-between items-center myblue text-white font-serif  font-semibold px-6 py-4 text-xl'>
+        <div className='flex items-center cursor-pointer'>
+          <FaUser size={28} className='text-[#232F3E] bg-white p-[2px] mx-3 rounded-full'/>
+          Hello, sign in
+        </div>
+         <IoMdClose onClick={handleNav} size={30} className=' mr-[-20px] cursor-pointer'/>
+      </div>
+      <div className='text-gray-600 font-semibold overflow-y-scroll h-[calc(100vh)]'>
+        <div  className='border-b-2'>
+          <h1 className='text-xl font-bold text-gray-800 py-6 px-9 pb-2'>
+            Digital Content & Devices
+          </h1>
+          <ul>
+            <li className='flex justify-between items-center hover:bg-slate-100 px-9 py-3 hover:cursor-pointer'>Amazon Music <IoIosArrowForward /></li>
+            <li className='flex justify-between items-center hover:bg-slate-100 px-9 py-3 hover:cursor-pointer'>Kindle E-readers & Books <IoIosArrowForward /></li>
+            <li className='flex justify-between items-center hover:bg-slate-100 px-9 py-3 mb-3 hover:cursor-pointer'>Amazon Appstore <IoIosArrowForward /></li>
+          </ul>
+        </div>
+        <div  className='border-b-2'>
+          <h1 className='text-xl font-bold text-gray-800 py-6 px-9 pb-2'>
+             Shop by Department
+          </h1>
+          <ul>
+            <li className='flex justify-between items-center hover:bg-slate-100 px-9 py-3 hover:cursor-pointer'>Electronics <IoIosArrowForward /></li>
+            <li className='flex justify-between items-center hover:bg-slate-100 px-9 py-3 hover:cursor-pointer'>Computers <IoIosArrowForward /></li>
+            <li className='flex justify-between items-center hover:bg-slate-100 px-9 py-3 hover:cursor-pointer'>Smart Home <IoIosArrowForward /></li>
+            <li className='flex justify-between items-center hover:bg-slate-100 px-9 py-3 mb-3 hover:cursor-pointer'>Arts & Crafts <IoIosArrowForward /></li>
+          </ul>
+        </div>
+        <div  className='border-b-2'>
+          <h1 className='text-xl font-bold text-gray-800 py-6 px-9 pb-2'>
+              Programs & Features
+          </h1>
+          <ul>
+            <li className='flex justify-between items-center hover:bg-slate-100 px-9 py-3 hover:cursor-pointer'>Gift Cards <IoIosArrowForward /></li>
+            <li className='flex justify-between items-center hover:bg-slate-100 px-9 py-3 hover:cursor-pointer'>Shop By Interest</li>
+            <li className='flex justify-between items-center hover:bg-slate-100 px-9 py-3 hover:cursor-pointer'>Amazon Live <IoIosArrowForward /></li>
+            <li className='flex justify-between items-center hover:bg-slate-100 px-9 py-3 mb-3 hover:cursor-pointer'>International Shopping <IoIosArrowForward /></li>
+          </ul>
+        </div>
+        <div  className='border-b-2'>
+          <h1 className='text-xl font-bold text-gray-800 py-6 px-9 pb-2'>
+             Help & Settings
+          </h1>
+          <ul>
+            <li className=' hover:bg-slate-100 px-9 py-3 hover:cursor-pointer'>Your Account</li>
+            <li className='flex items-center hover:bg-slate-100 px-9 py-3 hover:cursor-pointer'><TbWorld size={20} className='mr-3'/> English</li>
+            <li className='flex items-center hover:bg-slate-100 px-9 py-3 hover:cursor-pointer'><LiaFlagUsaSolid size={20} className='mr-3'/> United States</li>
+            <li className=' hover:bg-slate-100 px-9 py-3 hover:cursor-pointer'>Cusromer Service</li>
+            <li className=' hover:bg-slate-100 px-9 py-3 mb-[7rem] hover:cursor-pointer'>Sign in</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    </div>
+  
+    
     </>
     
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import headset from '../assets/headset.webp'
+import keyboard from '../assets/keyboard2.webp'
+import mouse from '../assets/mouse.webp'
+import chair from '../assets/chair.webp'
+import dining from '../assets/dining.webp'
+import home from '../assets/home.webp'
+import kitchen from '../assets/kichen.webp'
+import beauty from '../assets/beauty.webp'
+import deal from '../assets/deals.webp'
+import phone from '../assets/phone.webp'
+import tab from '../assets/tab.jpg'
+import pc from '../assets/pc.webp'
+import tv from '../assets/tv.webp'
+
 
 const Home = () => {
   const [products, setProducts] = useState([]);
   const [product2, setProduct2] = useState([]);
   const [product3, setProduct3] = useState([]);
   const [product4, setProduct4] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const imageUrls = [
+    'url("src/assets/bg1.webp")',
+    'url("src/assets/bg4.webp")',
+    'url("src/assets/bg2.webp")',
+  ];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+    }, 5000);
+
+    
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const backgroundImageStyle = {
+    backgroundImage: `linear-gradient(to bottom, transparent, rgb(223, 223, 223)), ${imageUrls[currentIndex]}`,
+    transition: 'background-image 0.5s ease-in-out'
+  };
 
 
   useEffect(() => {
@@ -53,8 +88,82 @@ const Home = () => {
   }, []);
 
   return (
-<div className=' w-full h-full py-4 px-4 bg-no-repeat bg-cover' style={{ backgroundImage: "linear-gradient(to bottom, transparent, rgb(223, 223, 223)), url('src/assets/bg1.webp')" }}>
-    <h1 className='py-20 px-20 pb-40 sm:text-4xl text-2xl lg:text-5xl font-bold text-gray-800'>Fill your bascket with Joy</h1>
+<div className='w-full h-auto object-cover py-4 bg-center bg-cover' style={backgroundImageStyle}>
+    <h1 className='py-20 px-20 pb-40 sm:text-4xl text-2xl lg:text-5xl font-extrabold text-gray-800'>Fill your bascket with Joy</h1>
+    <div className='grid grid-cols-4 gap-5 p-6'>
+      <div className='bg-white rounded-sm h-[29rem]'>
+        <h1 className='text-2xl font-bold p-6 pb-4'>Gaming accessories</h1>
+        <div className='grid grid-cols-2 mb-3'>
+          <div>
+            <img className='bg-white w-full h-[8rem] p-1' src={headset} alt="/" />
+            <p className='text-sm font-semibold px-4'>Headsets</p>
+          </div>
+          <div>
+            <img className='bg-white w-full h-[8rem] py-4' src={keyboard} alt="/" />
+            <p className='text-sm font-semibold px-4'>Keyboards</p>
+          </div>
+          <div className='mt-8'>
+            <img className='bg-white w-full h-[8rem] p-2' src={mouse} alt="/" />
+            <p className='text-sm font-semibold px-4'>Computer mice</p>
+          </div>
+          <div className='mt-8'>
+            <img className='bg-white w-full h-[8rem] p-1' src={chair} alt="/" />
+            <p className='text-sm font-semibold px-4'>Chairs</p>
+          </div> 
+        </div>
+        <a className='text-sm p-6 text-blue-700 font-semibold hover:text-red-800' href="#">See more</a>
+      </div>
+      <div className='bg-white rounded-sm h-[29rem]'>
+        <h1 className='text-2xl font-bold p-6 pb-4'>Refresh your space</h1>
+        <div className='grid grid-cols-2 gap-3 px-6'>
+          <div>
+            <img className='w-full h-[8rem]' src={dining} alt="/" />
+            <p className='text-sm font-semibold'>Dining</p>
+          </div>
+          <div>
+            <img className='w-full h-[8rem]' src={home} alt="/" />
+            <p className='text-sm font-semibold'>Home</p>
+          </div>
+          <div className='my-4 mt-3'>
+            <img className='w-full h-[8rem]' src={kitchen} alt="/" />
+            <p className='text-sm font-semibold'>Kitchen</p>
+          </div>
+          <div className='my-4 mt-3'>
+            <img className='w-full h-[8rem]' src={beauty} alt="/" />
+            <p className='text-sm font-semibold'>Health and Beauty</p>
+          </div> 
+        </div>
+        <a className='text-sm p-6 text-blue-700 font-semibold hover:text-red-800' href="#">See more</a>
+      </div>
+      <div className='bg-white rounded-sm h-[29rem]'>
+        <h1 className='text-2xl font-bold p-6 pb-4'>Deals in PCs</h1>
+        <img className='p-6  pb-9' src={deal} alt="/" />
+        <a className='text-sm px-6 text-blue-700 font-semibold hover:text-red-800' href="#">Shop now</a>
+      </div>
+      <div className='bg-white rounded-sm h-[29rem]'>
+        <h1 className='text-2xl font-bold p-6 pb-4'>Amazon Gadget Store</h1>
+        <div>
+            <img className='px-6 h-40 w-full' src={phone} alt="/" />
+            <p className='px-6 font-semibold'>Smartphones</p>
+          </div>
+        <div className='flex gap-1 px-6 py-4 pb-7'>
+          <div>
+            <img className='w-full h-20' src={tab} alt="/" />
+            <p className='font-semibold'>Tablets</p>
+          </div>
+          <div>
+            <img className='w-full h-20' src={pc} alt="/" />
+            <p className='font-semibold'>Laptops</p>
+          </div>
+          <div>
+            <img className='w-full h-20' src={tv} alt="/" />
+            <p className='font-semibold'>TVs</p>
+          </div> 
+        </div>
+        <a className='text-sm px-6 text-blue-700 font-semibold hover:text-red-800' href="#">See more from Amazon Gadgets</a>
+      </div>
+
+    </div>
     <div className='grid sm:grid-cols-2 gap-6'>
         <div className='bg-white rounded-lg md:ml-6'>
             <h1 className='font-bold text-2xl px-6 pt-3'>Men's Clothing</h1>
